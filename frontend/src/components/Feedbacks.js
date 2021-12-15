d0620952f8f3c694b5ca1e8afb07964f9ad18f24
Benedictus Wicaksono <benedictusbimo11@gmail.com>
2021-12-15 13:21:38 +0700

    Update frontend
    Add rating filter without backend

--- frontend/src/components/Feedbacks.js
+++ frontend/src/components/Feedbacks.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { AiFillStar } from "react-icons/ai";
+import { FaStar } from "react-icons/fa";
 import './Style.css';
 import "./components-css/Feedbacks.css";
 
 const Feedbacks = () => {
     const [feedbacks, setFeedbacks] = useState([]);
+    const [currentValue, setCurrentValue] = useState(0);
+    const [hoverValue, setHoverValue] = useState('');
+    const stars = Array(5).fill(0);
 
     useEffect(() => {
         getFeedbacks();
@@ -18,17 +21,50 @@ const Feedbacks = () => {
         setFeedbacks(response.data);
     }
 
+    const handleClick = value => {
+        setCurrentValue(value)
+        console.log(value)
+    };
+
+    const handleMouseOver = newHoverValue => {
+        setHoverValue(newHoverValue)
+    };
+
+    const handleMouseLeave = () => {
+        setHoverValue(undefined)
+    };
+
     return (
         <div className="feedbacks-container">
             <div className="feedbacks-wrapper">
-                <h2 class="container-title">Feedbacks</h2>
+                <div className="feedbacks-container-header">
+                    <h2 className="container-title">Feedbacks</h2>
+                    <div className="feedbacks-rating-upper-container">
+                        <div className='feedbacks-rating-container'>
+                            {stars.map((_, index) => {
+                                return (
+                                    <FaStar
+                                    key={index}
+                                    size={20}
+                                    onClick={() => handleClick(index + 1)}
+                                    onMouseOver={() => handleMouseOver(index + 1)}
+                                    onMouseLeave={handleMouseLeave}
+                                    color={(hoverValue || currentValue) > index ? "#ffc107" : "#a9a9a9"}
+                                    className='isiFeedbacks-rating-star'
+                                    />
+                                )
+                            })}
+                        </div>
+                        <button onClick={() => handleClick(0)} onMouseOver={() => handleMouseOver(0)} className="feedbacks-rating-toggle"><h3>Tampilkan Semua</h3></button>
+                    </div>
+                </div>
                 <ul className="feedbacks-auto-grid">
                     { feedbacks.map((feedbacks, index) => (
-                        <li className="feedbacks-card">
+                        <li key={index} className="feedbacks-card">
                             <div className="fb-card-content">
                                 <div className="fb-card-header">
                                     <h4>{feedbacks.name}</h4>
-                                    <h4 className="fb-card-header-star">{feedbacks.star}&#160;<AiFillStar color='#ffc107'/></h4>
+                                    <h4 className="fb-card-header-star">{feedbacks.star}&#160;<FaStar color='#ffc107'/></h4>
                                 </div>
                                 <p>{feedbacks.message}</p>
                             </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from "./App.js";
-// import axios from "axios";
-import FeedbacksPerusahaan from './components/FeedbacksPerusahaan.js';
-import Navbar from './components/Navbar.js';
-// axios.defaults.withCredentials = true;
+import App from "./App.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Navbar/>
-    <FeedbacksPerusahaan/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
